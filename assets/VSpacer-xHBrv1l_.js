dbc84e2c2b90d44235803f9ffae68058a537d7e2
tarsweb <tarsweb@users.noreply.github.com>
2024-05-24 16:23:32 +0000

    deploy: 3f24bfc6a64db45bf752ea30ccab56147e1b0841

---
+++ assets/VSpacer-xHBrv1l_.js
@@ -0,0 +1,1 @@
+import"./VGrid-Db5IO_WL.js";import{j as r}from"./index-BHtxuQah.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ assets/apiPhoto-DcVcg5h3.js
@@ -0,0 +1,1 @@
+import{bf as o}from"./index-J3b4gt5S.js";const a="/photos";async function s(){const{data:t}=await o.get(`${a}/`);return t}export{s as g};

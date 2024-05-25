a3c61ff1145c069d1cfae2674d576dbc9d5fba83
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-05-25 23:45:14 +0300

    update to main

--- src/components/NewPhotoForm.vue
+++ src/components/NewPhotoForm.vue
@@ -1,50 +1,51 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <v-btn
-        icon="mdi-close"
-        @click="$emit('close')"
-        position="absolute top-0 right-0"
-        variant="text"
-      />
-      <h1>Add New Photo</h1>
-      <v-card flat class="mx-auto px-6 py-8" width="500">
-        <v-form v-model="form" @submit.prevent="onSubmit">
-          <v-text-field
-            v-model="formData.title"
-            :readonly="isLoading"
-            label="Title"
-            :rules="rules['title']"
-            placeholder="Enter title"
-            :clearable="!isLoading"
-            variant="outlined"
-            prepend-inner-icon="mdi-pencil-outline"
-            required
-          />
-          <v-text-field
-            v-model="formData.description"
-            :readonly="isLoading"
-            :rules="rules['description']"
-            placeholder="Enter description"
-            label="Description"
-            multi-line
-            :clearable="!isLoading"
-            variant="outlined"
-            prepend-inner-icon="mdi-pencil-outline"
-            required
-          />
+      <v-sheet v-if="!isLoading" class="align-center" width="500">
+        <v-btn
+          icon="mdi-close"
+          @click="$emit('close')"
+          position="absolute top-0 right-0"
+          variant="text"
+        />
+        <h1>Add New Photo</h1>
+        <v-card flat class="mx-auto px-6 py-8" width="500">
+          <v-form v-model="form" @submit.prevent="onSubmit">
+            <v-text-field
+              v-model="formData.title"
+              :readonly="isLoading"
+              label="Title"
+              :rules="rules['title']"
+              placeholder="Enter title"
+              :clearable="!isLoading"
+              variant="outlined"
+              prepend-inner-icon="mdi-pencil-outline"
+              required
+            />
+            <v-text-field
+              v-model="formData.description"
+              :readonly="isLoading"
+              :rules="rules['description']"
+              placeholder="Enter description"
+              label="Description"
+              multi-line
+              :clearable="!isLoading"
+              variant="outlined"
+              prepend-inner-icon="mdi-pencil-outline"
+              required
+            />
 
-          <div class="d-flex flex-column justify-center my-4">
-            <v-img
-              :src="imageUrl"
-              width="50px"
-              height="50px"
-              class="mx-auto my-4"
-              lazy-src="https://picsum.photos/id/11/10/6"
-            >
-              <template v-slot:placeholder>
-                <div class="d-flex align-center justify-center fill-height">
-                  <!-- <v-file-input v-if="!formData.file"
+            <div class="d-flex flex-column justify-center my-4">
+              <v-img
+                :src="imageUrl"
+                width="50px"
+                height="50px"
+                class="mx-auto my-4"
+                lazy-src="https://picsum.photos/id/11/10/6"
+              >
+                <template v-slot:placeholder>
+                  <div class="d-flex align-center justify-center fill-height">
+                    <!-- <v-file-input v-if="!formData.file"
                   v-model="formData.file"
                   :readonly="isLoading"
                   :rules="rules['file']"
@@ -56,26 +57,26 @@
                   @update:model-value="onFileChange"
                 /> -->
 
-                  <!-- <v-progress-circular
+                    <!-- <v-progress-circular
                   
                   indeterminate
                   color="grey-lighten-5"
                 /> -->
-                </div>
-              </template>
-            </v-img>
-          </div>
-          <v-file-input
-            v-model="formData.file"
-            :readonly="isLoading"
-            :rules="rules['file']"
-            label="File photo"
-            :clearable="!isLoading"
-            prepend-icon="mdi-camera"
-            @update:model-value="onFileChange"
-            required
-          />
-          <!-- <v-select
+                  </div>
+                </template>
+              </v-img>
+            </div>
+            <v-file-input
+              v-model="formData.file"
+              :readonly="isLoading"
+              :rules="rules['file']"
+              label="File photo"
+              :clearable="!isLoading"
+              prepend-icon="mdi-camera"
+              @update:model-value="onFileChange"
+              required
+            />
+            <!-- <v-select
             v-model="formData.tags"
             :items="tags"
             :readonly="isLoading"
@@ -86,28 +87,29 @@
             chips
             density="comfortable"
           /> -->
-          <v-text-field 
-          v-model="formData.tags"
-          :readonly="isLoading"
-          label="Tags"
-          placeholder="Enter tags"
-          :clearable="!isLoading"
-          variant="outlined"
-          prepend-inner-icon="mdi-tag"
-          />
-          <v-btn
-            :disabled="!form"
-            :loading="isLoading"
-            block
-            color="success"
-            size="large"
-            type="submit"
-            prepend-icon="mdi-upload"
-          >
-            Upload
-          </v-btn>
-        </v-form>
-      </v-card>
+            <v-text-field
+              v-model="formData.tags"
+              :readonly="isLoading"
+              label="Tags"
+              placeholder="Enter tags"
+              :clearable="!isLoading"
+              variant="outlined"
+              prepend-inner-icon="mdi-tag"
+            />
+            <v-btn
+              :disabled="!form"
+              :loading="isLoading"
+              block
+              color="success"
+              size="large"
+              type="submit"
+              prepend-icon="mdi-upload"
+            >
+              Upload
+            </v-btn>
+          </v-form>
+        </v-card>
+      </v-sheet>
     </v-responsive>
   </v-container>
 </template>
